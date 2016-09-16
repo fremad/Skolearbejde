9a2a2d614c2a4d4fc5f1c9dfa87e670bbd1a16d2
Lars Hjerrild <201409555@post.au.dk>
2016-09-16 16:52:46 +0200

    it all

---
+++ I4GUI/GUIdatabinding/GUIdatabinding/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUIdatabinding
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        I4GUI.Agents agentGrp = new I4GUI.Agents();
+
+        private void Application_Startup(object s, StartupEventArgs e)
+        {
+            I4GUI.Agent agent1 = new I4GUI.Agent("007", "James Bond", "Park Avenue", "Assaniation", "Bush");
+            agentGrp.Add(agent1);
+        }
+        
+    }
+}
